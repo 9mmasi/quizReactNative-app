0f0925937276d516971b07e42fceb0810e7e20c1
9mmasi <mmasiinnocent@gmail.com>
2023-12-13 08:54:11 +0300

    add it all

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text ,StyleSheet,Image,TouchableOPacity} from 'react-native'
 import React from 'react'
+import Title from '../components/Title'
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Title />
+      <View style={styles.bannerContainer}>
+        <Image resizeMode='contain' style={styles.banner} source={require('../assets/test.png')} />
+      </View>
+      <TouchableOpacity>
+        <Text>Start</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  banner: {
+    height:300,
+    width:300
+} , 
+bannerContainer:{
+  alignItems:'center',
+}
+});
