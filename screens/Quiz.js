18ed2162dd9daefdddb3c9060e072722bf77eca4
9mmasi <mmasiinnocent@gmail.com>
2023-12-15 08:59:11 +0300

    push it

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -1,37 +1,49 @@
 import { View, TouchableOpacity,Text,StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 
 const Quiz = ({navigation}) => {
+  const[questions,setQuestions]=useState([])
+  const getQuiz=async()=>{
+    const baseUrl='https://opentdb.com/api.php?amount=10&type=multiple';
+    const res=await fetch(baseUrl)
+    const data = res.json()
+    console.log(data)
+    setQuestions(data)
+  }
+  useEffect(()=>{
+    getQuiz()
+  }
+    
+
+  ,[])
   return (
     <View style={styles.container}>
       <View style={styles.top}>
-        <Text>Imagine these is the real cool question</Text>
+        <Text style={styles.question}>Q.Imagine these is the real cool question</Text>
       </View>
       <View style={styles.optional}>
-        <TouchableOpacity>
-          <Text>Optional 1</Text>
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.option}>Optional 1</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Optional 2</Text>
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.option}>Optional 2</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Optional 3</Text>
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.option}>Optional 3</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>Optional 4</Text>
+        <TouchableOpacity style={styles.optionButton}>
+          <Text style={styles.option}>Optional 4</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.bottom}>
-      <TouchableOpacity>
-          <Text>Skip</Text>
+      <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Skip</Text>
         </TouchableOpacity>
       
-      <TouchableOpacity>
-          <Text>Next</Text>
+      <TouchableOpacity style={styles.button}>
+          <Text style={styles.text}>Next</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>navigation.navigate('Home')}>
-          <Text>End</Text>
-        </TouchableOpacity>
+        
       </View>
     </View>
   )
@@ -58,6 +70,35 @@ const styles = StyleSheet.create({
     justifyContent:'space-between',
     flexDirection:'row'
 
-  }
+  },
+  button:{
+    backgroundColor:'#314357',
+    alignItems:'center',
+    justifyContent:'center',
+    marginBottom:12,
+    borderRadius:16,
+    padding:14,
+    
+  
+  },
+  text:{
+    color:'#fff',
+    fontSize:14,
+    fontWeight:'600'
+  },
+  option:{
+    fontSize:18,
+    color:'#fff'
+  },
+  question:{
+    fontSize:28,
+  },
+  optionButton:{
+    paddingVertical:12,
+    marginVertical:6,
+    backgroundColor:'#314357',
+    paddingHorizontal:12,
+    borderRadius:12
+  },
 
 });
